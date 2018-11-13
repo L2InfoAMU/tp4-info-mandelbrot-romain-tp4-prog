8874a2a3988ebfe01def4f22992a3135c39de33a
john <williamgibson@laposte.net>
2018-11-13 19:49:10 +0100

    Update de quelques tests

--- src/main/java/mandelbrot/Complex.java
+++ src/main/java/mandelbrot/Complex.java
@@ -101,7 +101,7 @@ public class Complex {
      * @return A complex <code>c</code> such that <code>this * c = ||this|| ** 2</code>
      */
     Complex conjugate() {
-        return new Complex(-this.real, this.imaginary);
+        return new Complex(this.real, -this.imaginary);
     }
 
     /**
@@ -133,7 +133,7 @@ public class Complex {
      * @return <code>||this|| ** 2</code>
      */
     double squaredModulus() {
-        return real * real * imaginary * imaginary;
+        return real * real + imaginary * imaginary;
     }
 
     /**
@@ -153,7 +153,8 @@ public class Complex {
      */
     Complex reciprocal() {
         if (this.equals(ONE)){
-            throw new ArithmeticException("divide by zero");
+            return ONE;
+            //throw new ArithmeticException("divide by zero");
         }
         double m = squaredModulus();
         return new Complex(real / m, imaginary / m);
